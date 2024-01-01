5346638a08c5cecaf2dd0829783ba86c42a7ce8c
Nikita Gorin <exprompar@gmail.com>
2024-01-01 19:39:08 +0300

    g

--- cpp/Learn/.day1.cpp
+++ cpp/Learn/.day1.cpp
@@ -4,6 +4,21 @@ using namespace std;
 
 int main()
 {
+    bool b = true;
+    bool bo{true};
     
+    char c = 'a';
+    char d {'a'};
     
+    wchar_t g = 'Л';
+    wcout << g << '\n';
+    cout << (int)(c == d) << '\n';
+    //cout << true == false << '\n'; error...
+
+    cout << "code of a is: " << (int)(char('a')) << '\n';
+    cout << "code of a is: " << (int)'a' << '\n';
+
+    cout << "Size of char is " << sizeof('a') << '\n'; // 1
+    cout << "Size of char is " << sizeof(g) << '\n'; // 2
+
 }
---
+++ cpp/Learn/int_types.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    // одно и то же
+    short i00 = INT16_MAX;
+    short int i01 = INT16_MIN;
+    signed short i02 = INT16_MAX;
+    signed short int i03 = INT16_MIN;
+
+    // одно и то же
+    int i10 = INT32_MAX;
+    signed i11 = INT32_MAX;
+    signed int i12 = INT32_MIN;
+
+    // одно и то же
+    long i20 = INT32_MIN;
+    long int i21 = INT32_MAX; 
+    signed long i22 = INT32_MAX;
+    signed long int i23 = INT32_MIN;
+
+    // одно и то же
+    long long i30 = INT64_MAX;
+    long long int i31 = INT64_MIN;
+    signed long long i32 = INT64_MAX;
+    signed long long int i33 = INT64_MIN;
+
+    cout << "Short size: " << sizeof(short) << '\n'; // 2(Win)
+    cout << "Int size: " << sizeof(int) << '\n'; // 4(Win)
+    cout << "Long size: " << sizeof(long) << '\n'; // 4(Win)
+    cout << "Long Long size: " << sizeof(long long) << '\n'; // 8(Win)
+
+}
